2c7d11c973b21d15f58d4d50779551fc86a8bd14
NishimuraKaori <usakoyama@gmail.com>
2019-06-20 10:39:07 +0900

    move require position

--- grove-gps.js
+++ grove-gps.js
@@ -1,12 +1,5 @@
-/*
-const nmea = require('node-nmea');
- 
-const raw = '$GPRMC,161006.425,A,7855.6020,S,13843.8900,E,154.89,84.62,110715,173.1,W,A*30';
-const data = nmea.parse(raw);
-
-*/
-const nmea = require('node-nmea');
 module.exports = function(RED) {
+    const nmea = require('node-nmea');
     function GroveGpsNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
